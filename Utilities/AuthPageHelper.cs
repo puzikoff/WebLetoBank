336d8d2bc3af062e96dfc20e5242ecdfb90bddcd
timofeevma <timofeevma@ekassir.com>
2015-01-23 18:27:19 +0300

    Add files

---
+++ Utilities/AuthPageHelper.cs
@@ -0,0 +1,48 @@
+﻿using OpenQA.Selenium;
+
+namespace WebLetoBank.Utilities
+{
+    public class AuthPageHelper
+    {      
+
+        public static void FillAuthenticationFields(string userName, string password)
+        {
+             Browser.GetDriver().FindElement(By.Id("username")).Clear();
+             Browser.GetDriver().FindElement(By.Id("username")).SendKeys(userName);
+             Browser.GetDriver().FindElement(By.Id("password")).Clear();
+             Browser.GetDriver().FindElement(By.Id("password")).SendKeys(password);
+        }
+
+        public static void RegistrationButtonClick()
+        {
+            Browser.GetDriver().FindElement(By.XPath("//button[3]")).Click();
+            Browser.WaitReadyState();
+        }
+
+        public static void Authentication(string userName, string password)
+        {
+            Browser.GetDriver().FindElement(By.Id("username")).Clear();
+            Browser.GetDriver().FindElement(By.Id("username")).SendKeys(userName);
+            Browser.GetDriver().FindElement(By.Id("password")).Clear();
+            Browser.GetDriver().FindElement(By.Id("password")).SendKeys(password);
+            Browser.GetDriver().FindElement(By.XPath("//button[1]")).Click();
+            Browser.WaitReadyState();
+        }
+
+        public static void EnterButtonClick(){
+            Browser.GetDriver().FindElement(By.XPath("//button[1]")).Click();
+            Browser.WaitReadyState();
+        }
+
+        public static void ForgotLoginPasswordButtonClick(){
+            Browser.GetDriver().FindElement(By.XPath("//button[2]")).Click();
+            Browser.WaitReadyState();
+        }
+
+        public static void SetProtectCodeCheckboxClick()        {
+            Browser.GetDriver().FindElement(By.Id("setprotectcode")).Click();
+            Browser.WaitReadyState();
+        }
+
+    }
+}
---
+++ Utilities/FilesPaths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLetoBank.Utilities
+{
+    public class FilesPaths
+    {
+        public const string SelfPageScreen = @"ScreenShots\Self.jpg";
+        
+        public const string ContractsPageScreen = @"ScreenShots\Contracts.jpg";
+        
+        public const string ContractPageScreen = @"ScreenShots\Contract.jpg";
+        
+        public const string PaymentsSchedulePageScreen = @"ScreenShots\ContractSchedule.jpg";
+        
+        public const string ContractServicesScreen = @"ScreenShots\ContractServices.jpg";
+        
+        public const string TimelinePageScreen = @"ScreenShots\Timeline.jpg";
+        
+        public const string TimelineEventPageScreen = @"ScreenShots\TimelineEvent.jpg";
+        
+        public const string ContractTimelineScreen = @"ScreenShots\ContractTimeline.jpg";
+        
+        public const string ContractTimelineEventScreen = @"ScreenShots\ContractTimelineEvent.jpg";
+        
+    }
+}
