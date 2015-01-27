cbb509ef23f7d9fbde35a16fcdf653a2da064503
timofeevma <timofeevma@ekassir.com>
2015-01-27 14:22:42 +0300

    ssasasas

---
+++ WebLetoBank/AccountRegistration.cs
@@ -0,0 +1,86 @@
+﻿using NLog;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using WebLetoBank.Utilities;
+using System.Collections.Generic;
+
+namespace WebLetoBank.Tests
+{
+    [TestFixture]
+    public class AccountRegistration : TestBase
+    {
+        public static Logger Log;
+        private const string BaseUrl = "https://mtest.ekassir.com:4443/personalcabinet/api/v1";
+        //private const string BaseUrl = "https://sb2.ekassir.com/personalcabinet/api/v1";
+        //private const string BaseUrl = "https://mobile.letobank.ru/personalcabinet/api/v1";
+        private const string Password = "Qwerty";        
+        private const string AccessCode = "123456";
+        private const string OTR = "334500";
+        private const string ProtectCode = "1234";
+       
+        [Test]
+
+        [TestCase("40817810100340001241", "5596623")]
+        [TestCase("40817810900340000798", "5589895")]
+        [TestCase("40817810000340000423", "5584009")]
+        [TestCase("40817810200340020850", "5799519")]
+        [TestCase("40817810900340031929", "5898964")]
+        [TestCase("40817810500340048228", "6006507")]
+        [TestCase("40817810400340083370", "6209928")]
+        [TestCase("40817810000340008001", "55956")]       
+                
+        public void AccountRegistrationTest(string AccountNumber, string Username)
+        {
+            Log = LogManager.GetCurrentClassLogger();
+            Log.Trace(" Navigate to authentication page");
+            Browser.Navigate(BaseUrl + "/self");
+            Log.Trace(" Filling authentication fields");            
+            AuthPageHelper.FillAuthenticationFields("u" + Username, Password);
+            Log.Trace(" Click Registration Button ");
+            AuthPageHelper.RegistrationButtonClick();
+            Log.Trace(" Agree with license terms");
+            RegistrationHelper.AcceptTermsButtonClick();
+            Log.Trace(" Choose registration with account");
+            RegistrationHelper.ChooseRegistrationType("account");
+            Log.Trace(" Enter Account Number and access code: " + AccountNumber + " " + AccessCode);
+            RegistrationHelper.EnterAccountCredentials(AccountNumber, AccessCode);
+            Log.Trace(" Click Next. Go to SMS code page");
+            RegistrationHelper.NextButtonClick();
+            if (Browser.GetDriver().FindElement(By.XPath("//li")).Displayed) {                 
+                IWebElement Error = Browser.GetDriver().FindElement(By.XPath("//li"));
+                Log.Error(" " + Error.Text);               
+            }       
+            Browser.WaitReadyState();
+            Log.Trace(" Enter SMS code(otp)");
+            RegistrationHelper.EnterOTR(OTR);
+            Log.Trace(" Click Next. Go to username page");
+            RegistrationHelper.NextButtonClick();
+            Browser.WaitReadyState();
+            if (Browser.GetDriver().FindElement(By.XPath("//li")).Displayed) {
+                IWebElement Error = Browser.GetDriver().FindElement(By.XPath("//li"));
+                Log.Error(" " + Error.Text);                
+            }
+            Log.Trace(" Enter username: " + "u" + Username);
+            RegistrationHelper.EnterUsername("u" + Username);
+            Log.Trace(" Delete existing username field");
+            Browser.ExecuteJavaScript("var logins = document.getElementById('logins')");
+            Browser.ExecuteJavaScript("logins.parentNode.removeChild(logins)");
+            Log.Trace(" Click Next. Go to password page");
+            RegistrationHelper.NextButtonClick();            
+            Browser.WaitReadyState();
+            Log.Trace(" Enter and confirm password");
+            RegistrationHelper.EnterPassword(Password, Password);           
+            Log.Trace(" Click Next. Go to protect code page");
+            RegistrationHelper.NextButtonClick();
+            Browser.WaitReadyState();
+            Log.Trace(" Fill protect code fields");
+            RegistrationHelper.EnterProtectCode(ProtectCode, ProtectCode);
+            Log.Trace(" Click dismiss setting protect code");
+            RegistrationHelper.DismissProtectCodeButtonClick();
+            Browser.WaitReadyState();
+        }
+     }
+}
+
---
+++ WebLetoBank/CardRegistration.cs
@@ -0,0 +1,80 @@
+﻿using NLog;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using WebLetoBank.Utilities;
+using System.Collections.Generic;
+
+namespace WebLetoBank.Tests
+{
+    [TestFixture]
+    public class CardRegistration : TestBase
+    {
+        public static Logger Log;
+        private const string BaseUrl = "https://mtest.ekassir.com:4443/personalcabinet/api/v1";
+        //private const string BaseUrl = "https://sb2.ekassir.com/personalcabinet/api/v1";
+        //private const string BaseUrl = "https://mobile.letobank.ru/personalcabinet/api/v1";       
+        private const string Password = "Qwerty";        
+        private const string AccessCode = "123456";
+        private const string OTR = "334500";
+        private const string ProtectCode = "1234";        
+
+        [Test]
+
+        [TestCase("55956", "4059920012243789")]
+
+        public void CardRegistrationTest(string CRM, string CardNumber)
+        {
+            Log = LogManager.GetCurrentClassLogger();
+            Log.Trace(" Navigate to authentication page");
+            Browser.Navigate(BaseUrl + "/self");
+            Log.Trace(" Filling authentication fields");
+            AuthPageHelper.FillAuthenticationFields("u" + CRM, Password);
+            Log.Trace(" Click Registration Button ");
+            AuthPageHelper.RegistrationButtonClick();
+            Log.Trace(" Agree with license terms");
+            RegistrationHelper.AcceptTermsButtonClick();
+            Log.Trace(" Choose registration with account");
+            RegistrationHelper.ChooseRegistrationType("card");
+            Log.Trace(" Enter Account Number and access code");
+            RegistrationHelper.EnterCardCredentials(CardNumber, AccessCode);
+            Log.Trace(" Click Next. Go to SMS code page");
+            RegistrationHelper.NextButtonClick();
+            if (Browser.GetDriver().FindElement(By.XPath("//li")).Displayed)
+            {
+                IWebElement Error = Browser.GetDriver().FindElement(By.XPath("//li"));
+                Log.Error(" " + Error.Text);
+            }
+            Browser.WaitReadyState();
+            Log.Trace(" Enter SMS code(otp)");
+            RegistrationHelper.EnterOTR(OTR);
+            Log.Trace(" Click Next. Go to username page");
+            RegistrationHelper.NextButtonClick();
+            Browser.WaitReadyState();
+            if (Browser.GetDriver().FindElement(By.XPath("//li")).Displayed)
+            {
+                IWebElement Error = Browser.GetDriver().FindElement(By.XPath("//li"));
+                Log.Error(" " + Error.Text);
+            }
+            Log.Trace(" Enter username");
+            RegistrationHelper.EnterUsername("u" + CRM);
+            Log.Trace(" Delete existing username field");
+            Browser.ExecuteJavaScript("var logins = document.getElementById('logins')");
+            Browser.ExecuteJavaScript("logins.parentNode.removeChild(logins)");
+            Log.Trace(" Click Next. Go to password page");
+            RegistrationHelper.NextButtonClick();
+            Browser.WaitReadyState();
+            Log.Trace(" Enter and confirm password");
+            RegistrationHelper.EnterPassword(Password, Password);
+            Log.Trace(" Click Next. Go to protect code page");
+            RegistrationHelper.NextButtonClick();
+            Browser.WaitReadyState();
+            Log.Trace(" Fill protect code fields");
+            RegistrationHelper.EnterProtectCode(ProtectCode, ProtectCode);
+            Log.Trace(" Click dismiss setting protect code");
+            RegistrationHelper.DismissProtectCodeButtonClick();
+            Browser.WaitReadyState();
+        }
+    }
+}
